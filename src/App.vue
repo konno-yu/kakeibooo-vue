9a8ffcbda5229b06b4e7cc1d03bc9d719d5edc16
konno-yu <yuta.konno.socie@gmail.com>
2020-05-19 23:33:00 +0900

    Appbarを描画できるようにAppを修正

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <div class="root">
+      <KakeiboooAppBar/>
+    </div>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { Vue, Component } from 'vue-property-decorator';
 
-export default Vue.extend({
-  name: 'App',
+import KakeiboooAppBar from '@/components/KakeiboooAppBar.vue';
 
+@Component({
   components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-});
+    KakeiboooAppBar,
+  }
+})
+export default class App extends Vue {};
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&display=swap');
+
+.root {
+  font-family: 'M PLUS Rounded 1c', sans-serif;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: "column";
+  background: #FFFFFF;
+}
+</style>
