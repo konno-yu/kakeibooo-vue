4c60e9447f105d79654c5613348f020b13e64e47
konno-yu <yuta.konno.socie@gmail.com>
2020-05-24 18:29:22 +0900

    食材一覧を表示するlistコンポーネントを作成

---
+++ src/components/foodstaff/FoodStaffList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <slot v-bind="slotProps" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+import FoodStaffListItem from '@/components/foodstaff/FoodStaffListItem.vue';
+
+@Component({
+    components: {
+        FoodStaffListItem
+    }
+})
+export default class FoodStaffList extends Vue {
+    @Prop({})
+    private listItems!: {name: string, count: number, unit:string, category: string, subCategory?:string}[];
+
+    get slotPlops() {
+        return {
+            listItems: this.$props.listItems,
+            onclick: this.onclick
+        }
+    }
+
+    onclick(selected: string) {
+    }
+}
+</script>
+
+<style scoped>
+</style>
