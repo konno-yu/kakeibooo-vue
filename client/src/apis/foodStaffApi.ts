5cb83058034b7667850223ccdcec933bc8668123
konno-yu <yuta.konno.socie@gmail.com>
2020-06-07 11:52:56 +0900

    PUTとDELETEを実装

--- client/src/apis/foodStaffApi.ts
+++ client/src/apis/foodStaffApi.ts
@@ -12,10 +12,10 @@ export const post = async (requestBody: {}) => {
     return await axios.post('http://localhost:3000/foodstaffs', requestBody);
 };
 
-export const updateById = async () => {
-
+export const updateById = async (id: number, requestBody: {}) => {
+    return await axios.put('http://localhost:3000/foodstaffs/' + id, requestBody)
 };
 
-export const deleteById = async () => {
-
+export const deleteById = async (id: number) => {
+    return await axios.delete('http://localhost:3000/foodstaffs/' + id);
 };
