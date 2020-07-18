f0b3bf0ad3ff6c07698ad86e443fcc31d454157d
konno-yu <yuta.konno.socie@gmail.com>
2020-07-18 20:21:04 +0900

    食費管理のサーバ側実装

--- server/src/expenses/expenses.controller.ts
+++ server/src/expenses/expenses.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
+import { ExpensesDto } from './expenses.dto';
 
 @Controller('expenses')
 export class ExpensesController {
@@ -13,5 +14,20 @@ export class ExpensesController {
     @Get(':month')
     getByMonth(@Param('month') month: number) {
         return this.expensesService.getByMonth(month);
+    }
+
+    @Get(':year/:month/:date')
+    getByDate(@Param('year') year: number, @Param('month') month: number, @Param('date') date: number) {
+        return this.expensesService.getByDate(year, month, date);
+    }
+
+    @Post()
+    post(@Body() data: ExpensesDto) {
+        return this.expensesService.post(data);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() data: Partial<ExpensesDto>) {
+        return this.expensesService.update(id, data);
     }
 }
---
+++ server/src/expenses/expenses.dto.ts
@@ -0,0 +1,5 @@
+export interface ExpensesDto {
+    id: number;
+    targetDate: Date;
+    cost: number;
+}
