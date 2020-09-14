25eb2fa42d5c15885920eca3dfdfca633eb72f08
konno-yu <yuta.konno.socie@gmail.com>
2020-09-15 00:07:12 +0900

    [db]：食費登録のUI修正に伴い、食費用テーブルの形式および初期データを修正

--- docker/db/init/1_init.sql
+++ docker/db/init/1_init.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS foodstaffs;
 DROP TABLE IF EXISTS expenses;
+DROP TABLE IF EXISTS receipts;
 
 -- 食材テーブル
 CREATE TABLE foodstaffs (
@@ -19,63 +20,10 @@ CREATE TABLE expenses (
     cost INTEGER NOT NULL
 );
 
-
-INSERT INTO foodstaffs (staff_name, staff_count, unit, input_date, category, sub_category) VALUES
-    ('豚こま', 100, 'g', current_date, 'fridge-top', NULL),
-    ('鶏むね', 150, 'g', current_date, 'fridge-top', NULL),
-    ('あいびき肉', 200, 'g', current_date, 'fridge-top', NULL),
-    ('枝豆', 1, '袋', current_date, 'fridge-top', NULL),
-    ('大根', 0.2, '本', current_date, 'fridge-bottom', 'vegetables'),
-    ('人参', 0.5, '本', current_date, 'fridge-bottom', 'vegetables'),
-    ('じゃがいも', 3, '個', current_date, 'fridge-bottom', 'vegetables'),
-    ('白菜',  1, '本', current_date, 'fridge-bottom', 'vegetables'),
-    ('きゅうり', 2, '個', current_date, 'fridge-bottom', 'vegetables'),
-    ('肉じゃが', 100, 'g', current_date, 'fridge-bottom', 'leftovers'),
-    ('水菜サラダ', 100, 'g', current_date, 'fridge-bottom', 'leftovers'),
-    ('カレー', 150, 'g', current_date, 'fridge-bottom', 'leftovers'),
-    ('ルイボスティー', 500, 'ml', current_date, 'fridge-bottom', 'others'),
-    ('卵',  5, '個', current_date, 'fridge-bottom', 'others'),
-    ('稲荷の皮', 3, '個', current_date, 'fridge-bottom', 'others'),
-    ('しお', 1, '本', current_date, 'seasoning', NULL),
-    ('パルスイート', 100, 'ml', current_date, 'seasoning', NULL),
-    ('コンソメ', 400, 'g', current_date, 'seasoning', NULL),
-    ('カップ麺', 10, '個', current_date, 'preserved', NULL),
-    ('パスタ', 1, '個', current_date, 'preserved', NULL),
-    ('うどん', 4, '個', current_date, 'preserved', NULL);
-
-INSERT INTO expenses (target_date, cost) VALUES
-    ('2020-06-01', 900),
-    ('2020-06-02', 1000),
-    ('2020-06-03', 890),
-    ('2020-06-04', 1500),
-    ('2020-06-05', 980),
-    ('2020-06-06', 1980),
-    ('2020-06-07', 2500),
-    ('2020-06-08', 700),
-    ('2020-06-09', 0),
-    ('2020-06-10', 500),
-    ('2020-06-11', 1980),
-    ('2020-06-12', 1500),
-    ('2020-06-13', 1200),
-    ('2020-06-14', 1000),
-    ('2020-06-15', 880),
-    ('2020-06-16', 780),
-    ('2020-06-17', 1800),
-    ('2020-06-18', 2100),
-    ('2020-06-19', 1900),
-    ('2020-06-20', 990),
-    ('2020-06-21', 550),
-    ('2020-06-22', 980),
-    ('2020-06-23', 1000),
-    ('2020-06-24', 880),
-    ('2020-06-25', 780),
-    ('2020-06-26', 1800),
-    ('2020-06-27', 2100),
-    ('2020-06-28', 1900),
-    ('2020-06-29', 990),
-    ('2020-06-30', 550),
-    ('2020-07-01', 900),
-    ('2020-07-02', 1000),
-    ('2020-07-03', 890),
-    ('2020-07-04', 1500),
-    ('2020-07-05', 980);
+-- レシート単位の食費テーブル
+CREATE TABLE receipts (
+    id SERIAL NOT NULL,
+    purchase_date DATE NOT NULL,
+    store VARCHAR(100) NOT NULL,
+    expense INTEGER NOT NULL
+);
